b8609afbab5b943bf31dca468c0c58a3507ab8af
jeyog <choijy8816@gmail.com>
2022-04-17 15:12:04 +0900

    Refactor: token값 final 필드로 지정

--- src/test/java/kr/ac/jj/algo/controller/AuthControllerTest.java
+++ src/test/java/kr/ac/jj/algo/controller/AuthControllerTest.java
@@ -50,6 +50,8 @@ public class AuthControllerTest {
     @MockBean
     private AuthService authService;
 
+    private final static String TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE5MTYyMzkwMjJ9.LYnkjSJXgSuPtmZI4eoMW4KsUC8XfpoWn9K_P8SnCmND3ak--WAWK2yjt1_VB4XpFXGByJ16gy-amGAeD1wvThNpDZ1O4NHGk1hOrY8v5hbqhHWQU5A1YVW-WeJdk1fzM_SgQNDR20gJMWqfStuUEzmbYhxhSO749Xg5tX-L8eXDA1F3MAhroTXz7PgdM-syZw_AtgQZiXQlfIwZO5zvbvDoSKztoKDcUCCXwITTmSucV6RXKAnaNahVn9HHiMjxSum1DmneVolJCGzDJtpx5_z4-UkoAf8EJ_Gm4BqY3fbUtIi4RpH1ttvJ42_xXwmV-yBX3CZ0Wwpcmn06nBNbDriK50MJxcSIwjgVgBceRHqkR1Ep7Uc4_TSe_bQFP0_QcGS5TwIjp6Dr9fl85BIj5aRSX3uMJsXuz-1oZheUAhjWcfHAEdWSKJrMXGQttXctVA4m7OSEmOA1Tqv3ZdWu9TYvDwKCI-A7yf_6TvFe5T-PsnDSyVoUOAB_omVuk5Rb";
+
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext,
                       RestDocumentationContextProvider restDocumentation) throws ServletException {
@@ -79,7 +81,7 @@ public class AuthControllerTest {
         AuthDTO.Response response = new AuthDTO.Response();
         response.setMessage("로그인에 성공하였습니다.");
         response.setUser(user);
-        response.setToken("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwIiwiaWF0IjoxNjUwMDk5MDIyLCJleHAiOjI1NTAwOTkwMjJ9.HXL8wPh2Yj41AB_yF-sK3a96XXPzAS3QaECsMDfVoQdUuUt0J1vcaR_sQAUwSLwSngDO_Z2qOXZ5vxMzGJxoJJLO1-QDoHCFUR2pWV8l0ufrmkNACNB2YZ8szo0pNs5ZNf5LoQU6O9zf6jU1zOucEng0aztQMCAwDdAxJmztsKnLN4lQYxjgJrP4n1kiajA_9yzIG6Z0SrMNkFmf_ZOK0gMlesCI8If_Z89uU2SVb5o4na-qXL4PaW4HQ37E6cAw_ez76-62dZJUyJspi1qozdNHU6MU-K1SQKEmmTCZUz2PFPNyvzfio-so5WkeQTAKMIy-kf_bheCDpEwHJfe7hvN0W03-SItrS2eYovz1aqW7c5w8X7hH7azsMrY_-Z84fsBGh9eIA4VXanOeKEL3Mhe3LjkRgydcWPR404J7oVpY_I-dZSyn__pZSCl3qe0isV7X0qEiVyB4sHevj4tMbUyT9GwxYm6_7c0bgzraKfISb5994Z-a-XQaZcPLR5jQ");
+        response.setToken(TOKEN);
         given(authService.authenticate(any(AuthDTO.Request.class))).willReturn(response);
 
         AuthDTO.Request request = new AuthDTO.Request();
