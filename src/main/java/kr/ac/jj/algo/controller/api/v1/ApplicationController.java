d4a34c209f027e402d43a7d000d781609f734910
jeyog <choijy8816@gmail.com>
2022-04-23 16:15:15 +0900

    Refactor: ApplicationDTO.Response -> ApplicationDTO로 변경

--- src/main/java/kr/ac/jj/algo/controller/api/v1/ApplicationController.java
+++ src/main/java/kr/ac/jj/algo/controller/api/v1/ApplicationController.java
@@ -4,7 +4,6 @@ import kr.ac.jj.algo.domain.User;
 import kr.ac.jj.algo.dto.ApplicationDTO;
 import kr.ac.jj.algo.service.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,14 +18,12 @@ public class ApplicationController {
     }
 
     @GetMapping
-    public ResponseEntity<ApplicationDTO.Response> getMyApplication(@AuthenticationPrincipal User user) {
-        ApplicationDTO.Response response = applicationService.loadApplicationByUser(user);
-        return ResponseEntity.ok(response);
+    public ApplicationDTO getMyApplication(@AuthenticationPrincipal User user) {
+        return applicationService.loadApplicationByUser(user);
     }
 
     @PatchMapping
-    public ResponseEntity<ApplicationDTO.Response> patchMyApplication(@AuthenticationPrincipal User user, @RequestBody ApplicationDTO.Request request) {
-        ApplicationDTO.Response response = applicationService.patchApplicationByUser(user, request);
-        return ResponseEntity.ok(response);
+    public ApplicationDTO patchMyApplication(@AuthenticationPrincipal User user, @RequestBody ApplicationDTO.Request request) {
+        return applicationService.patchApplicationByUser(user, request);
     }
 }
--- src/main/java/kr/ac/jj/algo/service/ApplicationService.java
+++ src/main/java/kr/ac/jj/algo/service/ApplicationService.java
@@ -8,8 +8,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class ApplicationService {
     private final ModelMapper modelMapper;
@@ -25,21 +23,15 @@ public class ApplicationService {
         return applicationRepository.save(application);
     }
 
-    public ApplicationDTO.Response loadApplicationByUser(User user) {
-        ApplicationDTO.Response response = new ApplicationDTO.Response();
+    public ApplicationDTO loadApplicationByUser(User user) {
         Application application = applicationRepository.findByUser(user).orElse(new Application(user, ""));
-        response.setMessage("지원서를 성공적으로 불러왔습니다.");
-        response.setApplication(modelMapper.map(application, ApplicationDTO.class));
-        return response;
+        return modelMapper.map(application, ApplicationDTO.class);
     }
 
-    public ApplicationDTO.Response patchApplicationByUser(User user, ApplicationDTO.Request request) {
-        ApplicationDTO.Response response = new ApplicationDTO.Response();
+    public ApplicationDTO patchApplicationByUser(User user, ApplicationDTO.Request request) {
         Application application = applicationRepository.findByUser(user).orElse(new Application(user, ""));
         application.setIntroduction(request.getIntroduction());
         application = applicationRepository.save(application);
-        response.setMessage("지원서를 성공적으로 변경하였습니다.");
-        response.setApplication(modelMapper.map(application, ApplicationDTO.class));
-        return response;
+        return modelMapper.map(application, ApplicationDTO.class);
     }
 }
