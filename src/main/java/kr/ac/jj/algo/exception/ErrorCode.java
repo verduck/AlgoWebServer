be44e82756afaeab836177259739b86f1a3527d3
jeyog <choijy8816@gmail.com>
2022-04-23 15:52:15 +0900

    Feat: 게시판 Not found 오류코드 추가

--- src/main/java/kr/ac/jj/algo/exception/ErrorCode.java
+++ src/main/java/kr/ac/jj/algo/exception/ErrorCode.java
@@ -7,7 +7,9 @@ public enum ErrorCode {
     AUTH_BAD_REQUEST(HttpStatus.BAD_REQUEST, "학번 또는 비밀번호가 일치하지 않습니다."),
     NO_TOKEN(HttpStatus.BAD_REQUEST, "토큰을 존재하지 않습니다."),
     EXPIRED_TOKEN(HttpStatus.BAD_REQUEST, "만료된 토큰입니다."),
+    BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "게시판을 찾을 수 없습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+
 
     private final HttpStatus status;
     private final String message;
