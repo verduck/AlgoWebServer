baa69618b7ea6c4a846adc9b8be45c8325378272
jeyog <choijy8816@gmail.com>
2022-04-23 16:24:18 +0900

    Refactor: 죽은 코드 삭제

--- src/main/java/kr/ac/jj/algo/dto/ApplicationDTO.java
+++ src/main/java/kr/ac/jj/algo/dto/ApplicationDTO.java
@@ -44,32 +44,4 @@ public class ApplicationDTO {
             this.introduction = introduction;
         }
     }
-
-    public static class Response {
-        private String message;
-        private ApplicationDTO application;
-
-        public Response() {}
-
-        public Response(String message, ApplicationDTO application) {
-            this.message = message;
-            this.application = application;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public ApplicationDTO getApplication() {
-            return application;
-        }
-
-        public void setApplication(ApplicationDTO application) {
-            this.application = application;
-        }
-    }
 }
